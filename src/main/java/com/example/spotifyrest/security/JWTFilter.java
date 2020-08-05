4ee5c543d8f240f0fbec39486f2def05608d4e47
premib <32558912+premib@users.noreply.github.com>
2020-08-05 15:48:52 +0530

    removed cors

--- src/main/java/com/example/spotifyrest/security/JWTFilter.java
+++ src/main/java/com/example/spotifyrest/security/JWTFilter.java
@@ -22,12 +22,6 @@ public class JWTFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
 
-    httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
-    httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
-    httpServletResponse.setHeader("Access-Control-Allow-Headers", "*");
-    httpServletResponse.addHeader("Access-Control-Expose-Headers", "xsrf-token");
-
     String token = jwtProvider.resolveToken(httpServletRequest);
     try {
         if (token != null && jwtProvider.validateToken(token)) {
