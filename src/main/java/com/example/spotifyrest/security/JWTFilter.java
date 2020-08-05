0bdbe684ece88940e4e451484a5a35c820057e40
premib <premib123@gmail.com>
2020-08-05 19:47:03 +0530

    change in cors policy

--- src/main/java/com/example/spotifyrest/security/JWTFilter.java
+++ src/main/java/com/example/spotifyrest/security/JWTFilter.java
@@ -22,7 +22,7 @@ public class JWTFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
 
-    httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:8080, https://hungry-chandrasekhar-88615e.netlify.app/");
+    httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
     httpServletResponse.setHeader("Access-Control-Allow-Methods", "*");
     httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
     httpServletResponse.setHeader("Access-Control-Allow-Headers", "*");
